508280a858854dbe255c2b41b333c9b46013b06a
Connor Claxton <czclaxton@gmail.com>
2019-08-14 23:39:42 -0500

    trying to get reducers working

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
 
 const AddedFeature = props => {
+  console.log("this one", props);
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
       <button className="button">X</button>
-      {props.feature.name}
+      {props.name}
     </li>
   );
 };
 
-export default AddedFeature;
+// export default AddedFeature;
+
+const mapStateToProps = state => {
+  return {
+    state
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  {}
+)(AddedFeature);
